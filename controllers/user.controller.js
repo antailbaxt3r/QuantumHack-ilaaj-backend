903568a66d498253efdde3ba33a80b683dbf5dff
Arjun Bajpai <arjunbajpaiwork@gmail.com>
2020-08-28 22:29:04 +0530

    typo fixing

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -206,7 +206,7 @@ module.exports.userData = async (req, res) => {
         })
         res.status(200).json({
             success: true,
-            user: user
+            user: users
         })
     } catch (e) {
         console.log(e)
